d2e7d15a664b5227882a2cbb9b6e9d0513b36d28
dulce-mata <dulce.mata@alumnos.udg.mx>
2024-08-24 23:30:15 -0600

    Add files via upload

---
+++ ode_pendulo_doble.m
@@ -0,0 +1,17 @@
+[t,x]=ode45(@MR_B,[0 10],[0 (35*pi)/180 0 0]);
+
+figure(1)
+subplot(2,1,1);
+plot(t,x(:,1));
+grid on
+title("theta 1");
+xlabel("tiempo");
+ylabel("posicion ");
+
+
+subplot(2,1,2);
+plot(t,x(:,2));
+grid on
+title("theta 2")
+xlabel("tiempo");
+ylabel("posicion");
